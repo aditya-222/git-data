764ae0c7d366143fe62a4176e5778d2e519fc0f7
aditya-222 <33357019+aditya-222@users.noreply.github.com>
2017-11-15 18:33:12 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-	url(r'allrepos/',views.allrepos , name = 'allrepos' ),
-]
